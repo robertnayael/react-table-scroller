71511171d60367edc0595ad6bbf32ecd3046a106
Robert Gulewicz <nayael@wp.pl>
2019-08-07 01:13:07 +0200

    Define initial state

---
+++ src/TableScroller/reducer/initialState.ts
@@ -0,0 +1,25 @@
+import { BoundingRect, TableScrollerState } from '../models';
+
+const emptyRect: BoundingRect = {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+}
+
+export const initialState: TableScrollerState = {
+    position: 0,
+    isScrolling: false,
+    columns: 0,
+    rects: {
+        mainWrapper: emptyRect,
+        contentWrapper: emptyRect,
+        scrollbar: emptyRect,
+    },
+    startPosition: {
+        absolute: null,
+        relativeToHandler: null,
+    },
+};
