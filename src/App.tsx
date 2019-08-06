f9fec377603ec4b9414eb5a102f07d2e1af41024
Robert Gulewicz <nayael@wp.pl>
2019-08-07 01:17:44 +0200

    Add sample table

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
+import './App.css';
+import { TableScroller } from './TableScroller/TableScroller';
+
+const rows = Array.from({ length: 10 }, (_, i) => i + 1);
+const columns = Array.from({ length: 30 }, (_, i) => i + 1);
+
 const App: React.FC = () => {
   return (
-    null
+    <TableScroller>
+      <table>
+        <thead>
+          <tr>
+            {columns.map(value => <th key={`th.${value}`}>{value}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            rows.map((row) =>
+              <tr key={`row.${row}`}>
+                {columns.map(col => <td key={`col.${row}.${col}`}><input type="text" /></td>)}
+              </tr>
+            )
+          }
+        </tbody>
+
+      </table>
+    </TableScroller>
   );
 }
 
